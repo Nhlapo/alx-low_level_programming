510e6123733e3dadb6a44fab5f1bdf652c2f74ad
Nhlapo <maphoskho@gmail.com>
2023-07-13 05:57:51 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,14 @@
 #include <stdio.h>
 
 int main(void) {
-        printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-	printf("Size of float: %lu byte(s)\n", sizeof(float));
-	printf("Size of double: %lu byte(s)\n", sizeof(double));
-	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
-	printf("Size of pointer: %lu byte(s)\n", sizeof(void *));
-	return (0);
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+    printf("Size of double: %lu byte(s)\n", sizeof(double));
+    printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+    printf("Size of pointer: %lu byte(s)\n", sizeof(void *));
+
+    return 0;
 }
