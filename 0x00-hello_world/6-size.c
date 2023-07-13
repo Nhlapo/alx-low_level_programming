b63e0043615286464e04407ee7663c3a8ba052f9
Nhlapo <maphoskho@gmail.com>
2023-07-13 06:03:07 -0700

    6m

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,8 @@
  */
 #include <stdio.h>
 
-int main(void) {
+int main(void) 
+{
     printf("Size of a char: %lu byte(s)\n", sizeof(char));
     printf("Size of an int: %lu byte(s)\n", sizeof(int));
     printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
