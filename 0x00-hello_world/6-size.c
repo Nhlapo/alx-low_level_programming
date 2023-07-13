bb0942e70b0e56ea26759795dd09873297d7ea04
Nhlapo <maphoskho@gmail.com>
2023-07-13 05:35:17 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,15 @@
-/**
- * main - entry point
- *
- * Description: Prints the size of various types on the machine architecture
- * and automatically detects the machine architecture to determine the size of
- * the pointer.
- *
- * Return: Always o (Success)
- */
-
 #include <stdio.h>
 
 int main(void)
 {
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of int: %ld byte(s)\n", sizeof(int));
-	printf("Size of long: %ld byte(s)\n", sizeof(long));
-	printf("Size of long long: %ld byte(s)\n", sizeof(long long));
-	printf("Size of float: %ld byte(s)\n", sizeof(float));
-	printf("Size of double: %ld byte(s)\n", sizeof(double));
-	printf("Size of long double: %ld byte(s)\n", sizeof(long double));
-	printf("Size of pointer: %ld byte(s)\n", sizeof(void *));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
+	printf("Size of double: %lu byte(s)\n", sizeof(double));
+	printf("Size of long double: %lu byte(s)\n", sizeof(long double));
+	printf("Size of pointer: %lu byte(s)\n", sizeof(void *));
 
 	return (0);
 }
-
