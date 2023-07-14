d61f3ab7bdd3b176a4b4856134937abd1fe9d30d
Nhlapo <maphoskho@gmail.com>
2023-07-14 05:50:26 -0700

    3-print_apl

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * main - A program that prints the, and then in uppercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
