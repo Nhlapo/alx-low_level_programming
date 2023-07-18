ff11cd7898b95bc37570200369eaf7b4f0db1955
Nhlapo <maphoskho@gmail.com>
2023-07-18 03:22:10 -0700

    0-pu

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This function prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Call the _putchar function to print the string */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	/* Return 0 to indicate success */
+	return (0);
+}
