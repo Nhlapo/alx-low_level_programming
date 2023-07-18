51d3d861174a8bc230f20ed64e4cd5470c19bfb2
Nhlapo <maphoskho@gmail.com>
2023-07-18 12:16:44 -0700

    100-times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 
 	if (n < 0 || n > 15)
 		return;
-	
+
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
@@ -20,7 +20,7 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				
+
 				if (result < 100)
 					_putchar(' ');
 
