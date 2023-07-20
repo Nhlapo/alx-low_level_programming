ad47796d91fa7bcdd21dc1341180ef21494cddc4
Nhlapo <maphoskho@gmail.com>
2023-07-20 03:34:45 -0700

    awoseome

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-	
-	// Commented out the problematic part causing the infinite loop
+
+	/* Commented out the problematic part causing the infinite loop
 	// while (i < 10)
 	// {
 	//		putchar(i);
 	// }
-
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
