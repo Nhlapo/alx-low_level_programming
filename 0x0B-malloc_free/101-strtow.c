ee58e84b37fedd1676ab8cf6a7398d5599285cfb
Nhlapo <maphoskho@gmail.com>
2023-08-07 01:52:05 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -35,30 +35,23 @@ int count_word(char *s)
 																																																																																																								                                                                                                                                                                                                }
 																																														                
 																																														                                                                                                        }
-															                
-															                
 															                                                return (w);
-																			                
 }
-                
+
 /**
- *                 
- *                  * **strtow - splits a string into words
- *                                  
- *                                   * @str: string to split
- *                                                   
- *                                                    *
- *                                                                    
- *                                                                     * Return: pointer to an array of strings (Success)
- *                                                                                     
- *                                                                                      * or NULL (Error)
- *                                                                                                      
- *                                                                                                       */
-                
+ *
+ ** **strtow - splits a string into words
+ *
+ ** @str: string to split
+ *
+ **
+ *
+ ** Return: poi* or NULL (Error)
+ *
+ **/
+  
 char **strtow(char *str)
-	                
 {
-	                
 	        char **matrix, *tmp;
 		                
 		        int i, k = 0, len = 0, words, c = 0, start, end;
