b8b3a0763b8750e637104c127c5365e6732d9518
Nhlapo <maphoskho@gmail.com>
2023-08-10 09:36:19 -0700

    m

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,7 +7,7 @@
  * @b: Size of memory block to be allocated
  *
  * Return: Pointer to the allocated memory
- * If malloc fails, the function causes normal process termination with a status value of 98
+ * Ifoc fails, the function causes normal process termination with a status value of 98
  */
 void *malloc_checked(unsigned int b)
 {
